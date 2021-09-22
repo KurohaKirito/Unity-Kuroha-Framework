9314d5d31046e3d3c4b911896a9f5774fcd1609b
KurohaKirito <KurohaKirito@gmail.com>
2021-09-22 15:16:18 +0800

    增加粒子特效工具

---
+++ Tool/Release/ParticleEffectProfiler/Scripts/Editor/MyParticleEffectUI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 将特效的性能数据显示到 Scene 窗口
+/// </summary>
+[CustomEditor(typeof(ParticleEffectScript))] 
+public class MyParticleEffectUI : Editor
+{
+    private readonly string[] labelArray = new string[20];
+
+    private void OnSceneGUI()
+    {
+        var particleEffectScript = (ParticleEffectScript) target;
+
+        var index = 0;
+        labelArray[index] = GetParticleEffectData.GetTextRuntimeMemorySize(particleEffectScript.gameObject);
+        labelArray[++index] = GetParticleEffectData.GetParticleSystemCount(particleEffectScript.gameObject);
+
+        if (EditorApplication.isPlaying)
+        {
+            labelArray[++index] = GetParticleEffectData.GetOnlyParticleEffectDrawCallStr();
+            labelArray[++index] = GetParticleEffectData.GetParticleCountStr(particleEffectScript);
+            labelArray[++index] = GetParticleEffectData.GetPixDrawAverageStr(particleEffectScript);
+            labelArray[++index] = GetParticleEffectData.GetPixActualDrawAverageStr(particleEffectScript);
+            labelArray[++index] = GetParticleEffectData.GetPixRateStr(particleEffectScript);
+        }
+
+        ShowUI(); 
+    }
+
+    void ShowUI()
+    {
+        //开始绘制GUI
+        Handles.BeginGUI();
+
+        //规定GUI显示区域
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        GUIStyle style = new GUIStyle();
+        style.richText = true;
+        style.fontStyle = FontStyle.Bold;
+
+        for (int i = 0; i < labelArray.Length; i++)
+		{
+            if (!string.IsNullOrEmpty(labelArray[i]))
+	        {
+		        GUILayout.Label(labelArray[i], style);
+	        }
+		}
+
+        GUILayout.EndArea();
+
+        Handles.EndGUI();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ParticleEffectScript particleEffectScript = (ParticleEffectScript)target;
+
+        string autoCullingTips = GetParticleEffectData.GetCullingSupportedString(particleEffectScript.gameObject);
+        if (!string.IsNullOrEmpty(autoCullingTips))
+        {
+            GUILayout.Label("ParticleSystem以下选项会导致无法自动剔除：", EditorStyles.whiteLargeLabel);
+            GUILayout.Label(autoCullingTips);
+        }
+    }
+}
