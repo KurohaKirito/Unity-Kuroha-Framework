fcf2a20db3ea37627ec2de2be0bda4a0212efeca
KurohaKirito <KurohaKirito@gmail.com>
2021-10-26 11:12:34 +0800

    优化动画压缩脚本

--- Tool/AssetTool/Editor/AssetBatchTool/AnimationClipCompress.cs
+++ Tool/AssetTool/Editor/AssetBatchTool/AnimationClipCompress.cs
@@ -1,47 +1,62 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using Kuroha.Util.RunTime;
 using UnityEditor;
-using UnityEngine;
 
 namespace Kuroha.Tool.Editor.AssetBatchTool
 {
+    /// <summary>
+    /// 压缩动画片段
+    /// </summary>
     public static class AnimationClipCheck
     {
         [MenuItem("Tools/Check Animation")]
         public static void Check()
         {
-            var guids = AssetDatabase.FindAssets("t:AnimationClip", new[] {
+            var guids = AssetDatabase.FindAssets("t:AnimationClip", new[]
+            {
                 "Assets"
             });
 
-            var index = 0;
             var total = guids.Length;
-            try
+            for (var index = 0; index < total; index++)
             {
-                foreach (var guid in guids)
+                Kuroha.GUI.Editor.ProgressBar.DisplayProgressBar("动画片段压缩中, 可能遇到动画片段较大的情况, 请耐心等候", $"{index + 1}/{total}", index + 1, total);
+                var assetPath = AssetDatabase.GUIDToAssetPath(guids[index]);
+                var extension = Path.GetExtension(assetPath);
+                if (extension == ".anim")
                 {
-                    EditorUtility.DisplayProgressBar("anim", "", (float)++index / total);
-                    var assetPath = AssetDatabase.GUIDToAssetPath(guid);
-                    var extension = Path.GetExtension(assetPath);
-                    if (extension == ".anim")
-                    {
-                        var filePath = Application.dataPath + assetPath.Substring(6);
-                        var content = File.ReadAllText(filePath);
-                        content = Regex.Replace(content, "[0-9]{1,10}\\.[0-9]{1,15}", x => {
-                                var dotIndex = x.Value.IndexOf(".", StringComparison.Ordinal);
-                                var delta = x.Value.Length - dotIndex;
-                                return delta > 5 ? x.Value.Remove(dotIndex + 5) : x.Value;
-                            }
-                        );
-                        File.WriteAllText(filePath, content);
-                    }
+                    var filePath = Path.GetFullPath(assetPath);
+                    var content = File.ReadAllText(filePath);
+                    content = Regex.Replace(content, "-?[0-9]{1,10}\\.[0-9]{1,15}", Compress);
+                    File.WriteAllText(filePath, content);
                 }
             }
-            finally
+        }
+
+        private static string Compress(Match match)
+        {
+            // 小数点后位数最大限制
+            const int DELTA_LIMIT = 4;
+            
+            // 小数点的位置
+            var dotIndex = match.Value.IndexOf(".", StringComparison.Ordinal);
+            
+            // 小数位的个数
+            var delta = match.Value.Length - dotIndex;
+            
+            // 缩小小数位数
+            var result = match.Value;
+            if (delta > DELTA_LIMIT + 1)
             {
-                EditorUtility.ClearProgressBar();
+                result = result.Remove(dotIndex + DELTA_LIMIT + 1);
             }
+            if (result == "0.0000" || result == "-0.0000")
+            {
+                result = "0";
+            }
+            return result;
         }
     }
 }
