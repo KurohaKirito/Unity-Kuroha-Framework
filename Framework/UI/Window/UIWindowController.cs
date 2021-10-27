5ba52992c285c5e59abd456f282d823e8d44d7c7
KurohaKirito <KurohaKirito@gmail.com>
2021-10-27 09:14:20 +0800

    添加 UI 框架

---
+++ Framework/UI/Window/UIWindowController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Kuroha.Framework.UI.Window
+{
+    /// <summary>
+    /// 抽象类, 所有 Controller 层的父类
+    /// </summary>
+    public class UIWindowController
+    {
+        /// <summary>
+        /// View 层
+        /// </summary>
+        protected UIWindowView baseView;
+        
+        /// <summary>
+        /// UI 名 (同时作为 UI 的唯一标识)
+        /// </summary>
+        public string Name { get; private set; }
+        
+        /// <summary>
+        /// 得到 UI 在 Hierarchy 面板中的游戏物体
+        /// </summary>
+        /// <returns></returns>
+        public GameObject UI => baseView.gameObject;
+
+        /// <summary>
+        /// 初始化窗口
+        /// </summary>
+        public virtual void Init(in UIWindowView view, in string prefabName)
+        {
+            baseView = view;
+            Name = prefabName;
+        }
+        
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        protected static void OnButtonClick_Close()
+        {
+            Kuroha.Framework.UI.Manager.UIManager.UI.Window.Close();
+        }
+
+        /// <summary>
+        /// 显示窗口
+        /// </summary>
+        public virtual void Display<T>(T content) { }
+        
+        /// <summary>
+        /// 隐藏窗口
+        /// </summary>
+        public virtual void Hide() { }
+    }
+}
---
+++ Util/RunTime/LogUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace Kuroha.Util.RunTime
+{
+    /// <summary>
+    /// 日志管理器
+    /// </summary>
+    public class LogUtil : MonoBehaviour
+    {
+        /// <summary>
+        /// 日志文件路径
+        /// </summary>
+        private string logFilePath;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Start()
+        {
+            var date = DateTime.Now.ToString("yyyy-MM-dd");
+            logFilePath = $"{Application.dataPath}/Config/Log/{date}.txt";
+        }
+
+        /// <summary>
+        /// 追加日志信息
+        /// </summary>
+        /// <param name="content">信息内容</param>
+        public void AppendLog(string content)
+        {
+            content = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} : {content}\n";
+            File.AppendAllText(logFilePath, content, Encoding.UTF8);
+        }
+    }
+}
