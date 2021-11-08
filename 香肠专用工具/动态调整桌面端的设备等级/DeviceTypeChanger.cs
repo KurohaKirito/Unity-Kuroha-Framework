201b5a51439185161dbe844491c8f773a17a306e
KurohaKirito <KurohaKirito@gmail.com>
2021-11-08 17:44:37 +0800

    添加香肠专用测试工具

---
+++ 香肠专用工具/动态调整桌面端的设备等级/DeviceTypeChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DeviceTypeChanger : MonoBehaviour
+{
+    public static DeviceLevel type = DeviceLevel.HIGH;
+    
+    [SerializeField]
+    private string typeName = "高端机";
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("高端机", GUILayout.Height(50), GUILayout.Width(120)))
+        {
+            type = DeviceLevel.HIGH;
+            typeName = "高端机";
+        }
+        
+        if (GUILayout.Button("中端机", GUILayout.Height(50), GUILayout.Width(120)))
+        {
+            type = DeviceLevel.MIDDLE;
+            typeName = "中端机";
+        }
+        
+        if (GUILayout.Button("低端机", GUILayout.Height(50), GUILayout.Width(120)))
+        {
+            type = DeviceLevel.LOW;
+            typeName = "低端机";
+        }
+    }
+}
