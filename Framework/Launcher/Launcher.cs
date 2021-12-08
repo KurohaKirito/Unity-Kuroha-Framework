bfc788a1c31658a7496c9d8dc528d758a7935f75
KurohaKirito <KurohaKirito@gmail.com>
2021-12-08 21:22:38 +0800

    分离启动器, 分为框架启动器和场景启动器

--- Framework/Launcher/Launcher.cs
+++ Framework/Launcher/Launcher.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Kuroha.Framework.AsyncLoad;
 using Kuroha.Framework.Audio;
 using Kuroha.Framework.Singleton;
@@ -11,159 +10,23 @@ namespace Kuroha.Framework.Launcher
         /// <summary>
         /// 单例
         /// </summary>
-        private static Launcher Instance => InstanceBase as Launcher;
+        public static Launcher Instance => InstanceBase as Launcher;
         
         /// <summary>
         /// 场景异步加载器
         /// </summary>
         private AsyncLoadScene asyncLoadScene;
-        
-        /// <summary>
-        /// Start 事件队列
-        /// </summary>
-        private Queue<IOnStart> onStartQueue;
-        
-        /// <summary>
-        /// OnDestroy 事件队列
-        /// </summary>
-        private Queue<IOnDestroy> onDestroyQueue;
-        
-        /// <summary>
-        /// OnApplicationQuit 事件队列
-        /// </summary>
-        private Queue<IOnApplicationQuit> onApplicationQuitQueue;
-        
-        /// <summary>
-        /// 初始化框架
-        /// </summary>
-        private async void Start()
-        {
-            await Instance.InitAsync();
-        }
-
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        protected sealed override void Init()
-        {
-            base.Init();
-        }
 
         /// <summary>
         /// [Async] 初始化
         /// </summary>
         public sealed override async Task InitAsync()
         {
-            LaunchAsyncLoadScene();
+            asyncLoadScene ??= new AsyncLoadScene();
+            asyncLoadScene.OnLaunch();
+            
             await AudioPlayManager.Instance.InitAsync();
             await BugReport.BugReport.Instance.InitAsync();
-
-            OnStart();
-            ExecuteOnStart();
-        }
-
-        /// <summary>
-        /// 可以通过 Register 一类的方法来注册 Unity 事件, 或者写一些初始化逻辑
-        /// </summary>
-        protected virtual void OnStart() { }
-
-        /// <summary>
-        /// 初始化场景异步加载器
-        /// </summary>
-        private void LaunchAsyncLoadScene()
-        {
-            if (asyncLoadScene == null)
-            {
-                asyncLoadScene = new AsyncLoadScene();
-                asyncLoadScene.OnLaunch();
-            }
-        }
-
-        /// <summary>
-        /// 销毁
-        /// </summary>
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-            
-            asyncLoadScene = null;
-            
-            ExecuteOnDestroy();
-        }
-
-        /// <summary>
-        /// 退出游戏
-        /// </summary>
-        protected override void OnApplicationQuit()
-        {
-            base.OnApplicationQuit();
-            
-            asyncLoadScene = null;
-            
-            ExecuteOnApplicationQuit();
-        }
-
-        /// <summary>
-        /// 执行 Start 事件队列
-        /// </summary>
-        private void ExecuteOnStart()
-        {
-            onStartQueue ??= new Queue<IOnStart>();
-            while (onStartQueue.Count > 0)
-            {
-                onStartQueue.Dequeue().OnStart();
-            }
-        }
-        
-        /// <summary>
-        /// 执行 OnDestroy 事件队列
-        /// </summary>
-        private void ExecuteOnDestroy()
-        {
-            onDestroyQueue ??= new Queue<IOnDestroy>();
-            while (onDestroyQueue.Count > 0)
-            {
-                onDestroyQueue.Dequeue().OnDestroy();
-            }
-        }
-        
-        /// <summary>
-        /// 执行 OnApplicationQuit 事件队列
-        /// </summary>
-        private void ExecuteOnApplicationQuit()
-        {
-            onApplicationQuitQueue ??= new Queue<IOnApplicationQuit>();
-            while (onApplicationQuitQueue.Count > 0)
-            {
-                onApplicationQuitQueue.Dequeue().OnApplicationQuit();
-            }
-        }
-        
-        /// <summary>
-        /// 注册 Start 事件
-        /// </summary>
-        protected void RegisterOnStart(IOnStart func)
-        {
-            onStartQueue ??= new Queue<IOnStart>();
-            onStartQueue.Enqueue(func);
-        }
-        
-        /// <summary>
-        /// 注册 OnDestroy 事件
-        /// </summary>
-        protected void RegisterOnDestroy(IOnDestroy func)
-        {
-            onDestroyQueue ??= new Queue<IOnDestroy>();
-            onDestroyQueue.Enqueue(func);
-        }
-        
-        /// <summary>
-        /// 注册 OnApplicationQuit 事件
-        /// </summary>
-        protected void RegisterOnApplicationQuit(IOnApplicationQuit func)
-        {
-            onApplicationQuitQueue ??= new Queue<IOnApplicationQuit>();
-            onApplicationQuitQueue.Enqueue(func);
         }
     }
 }
---
+++ Framework/Launcher/SceneLauncher.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Kuroha.Framework.Launcher
+{
+    public class SceneLauncher : MonoBehaviour
+    {
+        /// <summary>
+        /// Start 事件队列
+        /// </summary>
+        private Queue<IOnStart> onStartQueue;
+        
+        /// <summary>
+        /// OnDestroy 事件队列
+        /// </summary>
+        private Queue<IOnDestroy> onDestroyQueue;
+        
+        /// <summary>
+        /// OnApplicationQuit 事件队列
+        /// </summary>
+        private Queue<IOnApplicationQuit> onApplicationQuitQueue;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        protected virtual void SceneStart() { }
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        protected virtual void RegisterEvent() { }
+        
+        /// <summary>
+        /// 启动游戏框架
+        /// </summary>
+        private static async Task LaunchFramework()
+        {
+            var launcher = GameObject.Find($"Singleton_{nameof(Launcher)}");
+            if (launcher == null)
+            {
+                await Launcher.Instance.InitAsync();
+            }
+        }
+
+        /// <summary>
+        /// Unity 事件: Start
+        /// </summary>
+        private async void Start()
+        {
+            // 启动游戏框架
+            await LaunchFramework();
+            
+            // 注册用户事件
+            RegisterEvent();
+            
+            // 执行用户注册的 Start 事件
+            ExecuteStart();
+            
+            // 执行场景的初始化
+            SceneStart();
+        }
+
+        /// <summary>
+        /// Unity 事件: OnDestroy
+        /// </summary>
+        private void OnDestroy()
+        {
+            ExecuteOnDestroy();
+        }
+
+        /// <summary>
+        /// Unity 事件: OnApplicationQuit
+        /// </summary>
+        private void OnApplicationQuit()
+        {
+            ExecuteOnApplicationQuit();
+        }
+        
+        /// <summary>
+        /// 执行 Start 事件队列
+        /// </summary>
+        private void ExecuteStart()
+        {
+            onStartQueue ??= new Queue<IOnStart>();
+            while (onStartQueue.Count > 0)
+            {
+                onStartQueue.Dequeue().OnStart();
+            }
+        }
+        
+        /// <summary>
+        /// 执行 OnDestroy 事件队列
+        /// </summary>
+        private void ExecuteOnDestroy()
+        {
+            onDestroyQueue ??= new Queue<IOnDestroy>();
+            while (onDestroyQueue.Count > 0)
+            {
+                onDestroyQueue.Dequeue().OnDestroy();
+            }
+        }
+        
+        /// <summary>
+        /// 执行 OnApplicationQuit 事件队列
+        /// </summary>
+        private void ExecuteOnApplicationQuit()
+        {
+            onApplicationQuitQueue ??= new Queue<IOnApplicationQuit>();
+            while (onApplicationQuitQueue.Count > 0)
+            {
+                onApplicationQuitQueue.Dequeue().OnApplicationQuit();
+            }
+        }
+        
+        /// <summary>
+        /// 注册 Start 事件
+        /// </summary>
+        protected void RegisterStart(IOnStart func)
+        {
+            onStartQueue ??= new Queue<IOnStart>();
+            onStartQueue.Enqueue(func);
+        }
+        
+        /// <summary>
+        /// 注册 OnDestroy 事件
+        /// </summary>
+        protected void RegisterOnDestroy(IOnDestroy func)
+        {
+            onDestroyQueue ??= new Queue<IOnDestroy>();
+            onDestroyQueue.Enqueue(func);
+        }
+        
+        /// <summary>
+        /// 注册 OnApplicationQuit 事件
+        /// </summary>
+        protected void RegisterOnApplicationQuit(IOnApplicationQuit func)
+        {
+            onApplicationQuitQueue ??= new Queue<IOnApplicationQuit>();
+            onApplicationQuitQueue.Enqueue(func);
+        }
+    }
+}
