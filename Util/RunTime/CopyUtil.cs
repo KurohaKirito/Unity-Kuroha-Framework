6c0f29163a8b49659436adbda70f1dad20c555bc
KurohaKirito <KurohaKirito@gmail.com>
2022-02-09 10:17:33 +0800

    临时提交 - 深拷贝和瓦片地图工具类

---
+++ Util/RunTime/CopyUtil.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Kuroha.Util.RunTime
+{
+    public static class CopyUtil
+    {
+        public static T DeepCopy<T>(T source) where T : class
+        {
+            if (typeof(T).IsSerializable == false)
+            {
+                DebugUtil.LogError($"The type must be serializable. {typeof(T)}", null, "red");
+            }
+
+            if (object.ReferenceEquals(source, null))
+            {
+                return default;
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(stream) as T;
+            }
+            
+            
+        }
+    }
+}
