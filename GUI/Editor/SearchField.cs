c89b9e9cd10a6c79fa3c93670ffc0c96f5a05cf0
KurohaKirito <KurohaKirito@gmail.com>
2022-03-01 12:56:29 +0800

    新增: 搜索框 UI

--- GUI/Editor/SearchField.cs
+++ GUI/Editor/SearchField.cs
@@ -1,67 +1,67 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace Script.Effect.Editor.AssetTool.GUI.Editor {
-    public class SearchField {
+namespace Script.Effect.Editor.AssetTool.GUI.Editor
+{
+    public class SearchField
+    {
         private GUIStyle textFieldRoundEdge;
         private GUIStyle textFieldRoundEdgeCancelButton;
         private GUIStyle textFieldRoundEdgeCancelButtonEmpty;
         private GUIStyle transparentTextField;
 
-        public string DrawSearchField(string inputSearchText) {
-            if (textFieldRoundEdge == null) {
+        public bool OnGUI(ref string text)
+        {
+            if (textFieldRoundEdge == null)
+            {
                 textFieldRoundEdge = new GUIStyle("SearchTextField");
                 textFieldRoundEdgeCancelButton = new GUIStyle("SearchCancelButton");
                 textFieldRoundEdgeCancelButtonEmpty = new GUIStyle("SearchCancelButtonEmpty");
-                transparentTextField = new GUIStyle(EditorStyles.whiteLabel) {
-                    normal = {
-                        textColor = EditorStyles.textField.normal.textColor
-                    }
-                };
+                transparentTextField = new GUIStyle(EditorStyles.whiteLabel) { normal = { textColor = EditorStyles.textField.normal.textColor } };
             }
 
-            // 获取当前输入框的 Rect(位置大小)
+            // 获取当前输入框的 Rect (位置大小)
             var position = EditorGUILayout.GetControlRect();
             // 设置圆角 style 的 GUIStyle
-            var localRoundEdge = this.textFieldRoundEdge;
+            var localRoundEdgeStyle = textFieldRoundEdge;
             // 设置输入框的 GUIStyle 为透明, 所以看到的输入框是 textFieldRoundEdge 的风格
-            var localTextField = this.transparentTextField;
+            var localTextFieldStyle = transparentTextField;
             // 选择取消按钮(x)的 GUIStyle
-            var gUIStyle = inputSearchText != ""? textFieldRoundEdgeCancelButton : textFieldRoundEdgeCancelButtonEmpty;
+            var guiStyle = string.IsNullOrEmpty(text)? textFieldRoundEdgeCancelButtonEmpty : textFieldRoundEdgeCancelButton;
 
             // 输入框的水平位置向左移动取消按钮宽度的距离
-            position.width -= gUIStyle.fixedWidth;
+            position.width -= guiStyle.fixedWidth;
+
             // 如果面板重绘
-            if (Event.current.type == EventType.Repaint) {
-                // 根据是否是专业版来选取颜色
-                UnityEngine.GUI.contentColor = (EditorGUIUtility.isProSkin? Color.black : new Color(0f, 0f, 0f, 0.5f));
-                // 当没有输入的时候提示请输入
-                localRoundEdge.Draw(position, string.IsNullOrEmpty(inputSearchText)? new GUIContent("请输入") : new GUIContent(""), 0);
-                // 因为是全局变量, 用完要重置回来
+            if (Event.current.type == EventType.Repaint)
+            {
+                UnityEngine.GUI.contentColor = EditorGUIUtility.isProSkin? Color.black : new Color(0f, 0f, 0f, 0.5f);
+                localRoundEdgeStyle.Draw(position, string.IsNullOrEmpty(text)? new GUIContent("Input Search Text") : new GUIContent(""), 0);
                 UnityEngine.GUI.contentColor = Color.white;
             }
 
             var rect = position;
-            // 为了空出左边那个放大镜的位置
-            var num = localRoundEdge.CalcSize(new GUIContent("")).x - 2f;
+            // 空出左边那个放大镜的位置
+            var num = localRoundEdgeStyle.CalcSize(new GUIContent("")).x - 2f;
             rect.width -= num;
             rect.x += num;
-            rect.y += 1f; // 为了和后面的 style 对其
+            // 和后面的 style 对齐
+            rect.y += 1f;
 
-            inputSearchText = EditorGUI.TextField(rect, inputSearchText, localTextField);
+            text = EditorGUI.TextField(rect, text, localTextFieldStyle);
+
             // 绘制取消按钮，位置要在输入框右边
             position.x += position.width;
-            position.width = gUIStyle.fixedWidth;
-            position.height = gUIStyle.fixedHeight;
-            if (UnityEngine.GUI.Button(position, GUIContent.none, gUIStyle) && inputSearchText != "") {
-                inputSearchText = "";
-                // 用户是否做了输入
+            position.width = guiStyle.fixedWidth;
+            position.height = guiStyle.fixedHeight;
+            if (UnityEngine.GUI.Button(position, GUIContent.none, guiStyle) && !string.IsNullOrEmpty(text))
+            {
+                text = string.Empty;
                 UnityEngine.GUI.changed = true;
-                // 把焦点移开输入框
                 GUIUtility.keyboardControl = 0;
             }
 
-            return inputSearchText;
+            return UnityEngine.GUI.changed;
         }
     }
 }
