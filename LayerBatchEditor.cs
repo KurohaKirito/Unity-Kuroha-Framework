d379613a50b4c8de5a7afc8ee1cee2bfb5f492c6
KurohaKirito <KurohaKirito@gmail.com>
2022-05-05 17:20:34 +0800

    皮肤检查工具

--- LayerBatchEditor.cs
+++ LayerBatchEditor.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Script.Effect.Editor.AssetTool.Util.Editor;
+using Script.Effect.Editor.AssetTool.Util.RunTime;
 using UnityEditor;
 using UnityEngine;
 
@@ -51,6 +51,32 @@ namespace Script.Effect.Editor.AssetTool
 
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh();
+                }
+            }
+
+            if (GUILayout.Button("生成全部预制体"))
+            {
+                var assetPath = PathUtil.GetAssetPath(filePath);
+                var guids = AssetDatabase.FindAssets("t:Prefab", new[] { assetPath });
+                DebugUtil.LogError($"一共找到了 {guids.Length} 个预制体", null, "yellow");
+
+                var counterX = 0;
+                var counterY = 0;
+                
+                foreach (var guid in guids)
+                {
+                    var path = AssetDatabase.GUIDToAssetPath(guid);
+                    var asset = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                    var prefab = Instantiate(asset);
+                    var transform = prefab.transform;
+                    transform.SetPositionAndRotation(new Vector3(counterX, counterY, 0), Quaternion.Euler(0, 90, 0));
+
+                    counterY++;
+                    if (counterY % 9 == 0)
+                    {
+                        counterY = 0;
+                        counterX += 2;
+                    }
                 }
             }
         }
