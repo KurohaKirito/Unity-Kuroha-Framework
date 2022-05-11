6a99d0525ddac0e7ab4f2d18dd730329ab41f07b
KurohaKirito <KurohaKirito@gmail.com>
2022-05-11 11:58:39 +0800

    优化 IeMeshRenderer

--- Tool/InspectorExtender/Editor/IeMeshRenderer.cs
+++ Tool/InspectorExtender/Editor/IeMeshRenderer.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Kuroha.Framework.Utility.RunTime;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,103 +12,121 @@ namespace Kuroha.Tool.InspectorExtender.Editor
     /// <summary>
     /// 扩展 Mesh Renderer
     /// </summary>
+    [CanEditMultipleObjects]
     [CustomEditor(typeof(MeshRenderer))]
     public class IeMeshRenderer : IeBase
     {
-        private bool meshFoldout = true;
         private Mesh selfMesh;
+        private Mesh selfMeshOld;
         private MeshFilter selfFilter;
         private MeshRenderer selfMeshRenderer;
-        private MeshRenderer selfMeshRendererOld;
-        
+
+        private bool meshFoldout = true;
         private bool rwEnable;
         private string meshPath;
         private ModelImporterMeshCompression compression;
         private bool revertButton;
         private bool applyButton;
         private readonly Regex regexReadWrite = new Regex(@"m_IsReadable: [\d]");
-        private readonly Regex regexCompression = new Regex(@"m_MeshCompression: [\d]");
 
-        private bool showLayerValue;
+        private bool layerSortFoldout = true;
         private readonly List<string> layerNames = new List<string>();
-        private const string BUTTON_TO_EDIT = "Edit Layer Value";
-        private const string BUTTON_TO_SAVE = "Save Layer Value";
 
-        /// <summary>
-        /// 构造方法
-        /// </summary>
         public IeMeshRenderer() : base("UnityEditor.MeshRendererEditor") { }
 
-        private void ReloadTarget()
+        private void ReloadComponent()
         {
-            selfMeshRendererOld = selfMeshRenderer;
-            
-            selfFilter = selfMeshRenderer.GetComponent<MeshFilter>();
+            selfMeshRenderer = target as MeshRenderer;
+            if (selfMeshRenderer != null)
+            {
+                selfFilter = selfMeshRenderer.GetComponent<MeshFilter>();
+            }
+        }
+
+        private void ReloadMesh()
+        {
+            selfMesh = selfFilter.sharedMesh;
+        }
+
+        private void ReloadSetting()
+        {
+            selfMeshOld = selfMesh;
+            meshPath = AssetDatabase.GetAssetPath(selfMesh);
+            rwEnable = selfMesh.isReadable;
+            compression = MeshUtility.GetMeshCompression(selfMesh);
+        }
+
+        private void UpdateTarget()
+        {
+            if (selfMeshRenderer == null || selfFilter == null)
+            {
+                ReloadComponent();
+            }
             
             if (selfFilter != null)
             {
-                selfMesh = selfFilter.sharedMesh;
+                ReloadMesh();
             }
 
-            if (selfMesh != null)
+            if (selfMesh != null && selfMesh != selfMeshOld)
             {
-                meshPath = AssetDatabase.GetAssetPath(selfMesh);
-                rwEnable = selfMesh.isReadable;
-                compression = MeshUtility.GetMeshCompression(selfMesh);
-                DebugUtil.Log("重新加载了 Mesh", this, "yellow");
+                ReloadSetting();
             }
         }
         
-        private void UpdateTarget()
-        {
-            selfMeshRenderer = target as MeshRenderer;
-            
-            if (selfFilter == null || selfMesh == null || selfMeshRenderer != selfMeshRendererOld)
-            {
-                ReloadTarget();
-            }
-        }
-
         private async void OnMeshGUI()
         {
             if (selfMesh == null)
             {
                 return;
             }
-            
-            meshFoldout = EditorGUILayout.Foldout(meshFoldout, "Mesh", true);
+
+            meshFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(meshFoldout, "Mesh");
             if (meshFoldout)
             {
                 OnApplyButtonGUI();
-                
+
                 if (revertButton)
                 {
-                    ReloadTarget();
+                    ReloadSetting();
                 }
-                
+
                 if (applyButton)
                 {
-                    await SetSettings();
+                    // 先修改压缩等级, 后修改读写, 顺序不能颠倒
+                    if (compression != MeshUtility.GetMeshCompression(selfMesh))
+                    {
+                        SetMeshCompression();
+                    }
+
+                    if (rwEnable != selfMesh.isReadable)
+                    {
+                        await SetMeshReadable();
+                    }
                 }
             }
         }
 
         private void OnApplyButtonGUI()
         {
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Current Mesh", selfMeshOld, typeof(Mesh), true);
+            GUI.enabled = true;
+            
             // Unity 内置资源不可修改
             if (meshPath.IndexOf("Assets", StringComparison.Ordinal) < 0)
             {
                 GUI.enabled = false;
             }
-            
-            rwEnable = EditorGUILayout.Toggle("Read / Write Enabled", rwEnable);
+
+            rwEnable = EditorGUILayout.Toggle("Read Write Enabled", rwEnable);
             compression = (ModelImporterMeshCompression) EditorGUILayout.EnumPopup("Mesh Compression", compression);
-            
+
             if (rwEnable == selfMesh.isReadable && compression == MeshUtility.GetMeshCompression(selfMesh))
             {
                 GUI.enabled = false;
             }
-                
+
             EditorGUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
             revertButton = GUILayout.Button("Revert");
@@ -117,125 +134,97 @@ namespace Kuroha.Tool.InspectorExtender.Editor
             EditorGUILayout.EndHorizontal();
             GUI.enabled = true;
         }
-        
-        private async Task SetSettings()
+
+        private async Task SetMeshReadable()
         {
             string meshData;
             var meshFullPath = Path.GetFullPath(meshPath);
-            DebugUtil.Log($"meshFullPath: {meshFullPath}");
+
             using (var reader = new StreamReader(meshFullPath))
             {
                 meshData = await reader.ReadToEndAsync();
                 var readWriteString = "m_IsReadable: " + (rwEnable ? 1 : 0);
-                var compressionString = "m_MeshCompression: " + Convert.ToInt32(compression);
                 meshData = regexReadWrite.Replace(meshData, readWriteString);
-                meshData = regexCompression.Replace(meshData, compressionString);
             }
-            
+
             using (var writer = new StreamWriter(meshFullPath))
             {
                 await writer.WriteAsync(meshData);
             }
-            
+
             AssetDatabase.Refresh();
-            ReloadTarget();
+            UpdateTarget();
         }
 
-        /// <summary>
-        /// 绘制 Inspector
-        /// </summary>
+        private void SetMeshCompression()
+        {
+            MeshUtility.SetMeshCompression(selfMesh, compression);
+            EditorUtility.SetDirty(selfMesh);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+
             UpdateTarget();
+
+            OnLayerSortGUI();
+
             OnMeshGUI();
+        }
 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            #region 绘制按钮
-
-            if (selfMeshRenderer != null)
+        private void OnLayerSortGUI()
+        {
+            layerSortFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(layerSortFoldout, "Layer");
+            if (layerSortFoldout)
             {
-                EditorGUILayout.BeginHorizontal();
-                
-                if (GUILayout.Button(showLayerValue ? BUTTON_TO_SAVE : BUTTON_TO_EDIT))
-                {
-                    showLayerValue = !showLayerValue;
-                }
-                
-                if (selfFilter == null)
-                {
-                    selfFilter = selfMeshRenderer.GetComponent<MeshFilter>();
-                }
+                ShowSortingLayer();
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+        }
 
-                if (selfFilter != null)
-                {
-                    var mesh = selfFilter.sharedMesh;
-                
-                    if (GUILayout.Button("Save Mesh..."))
-                    {
-                        SaveMesh(mesh, mesh.name, false, true);
-                    }
-                
-                    if (GUILayout.Button("Save Mesh As New Instance..."))
-                    {
-                        SaveMesh(mesh, mesh.name, true, true);
-                    }
-                }
-                
-                EditorGUILayout.EndHorizontal();
+        private void ShowSortingLayer()
+        {
+            if (layerNames.Count <= 0 || layerNames.Count != SortingLayer.layers.Length)
+            {
+                layerNames.Clear();
+                layerNames.AddRange(SortingLayer.layers.Select(layer => layer.name));
             }
 
-            #endregion
+            // 显示 Sorting Layer
+            var layerValue = SortingLayer.GetLayerValueFromID(selfMeshRenderer.sortingLayerID);
+            layerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, layerNames.ToArray());
+            selfMeshRenderer.sortingLayerName = SortingLayer.layers[layerValue].name;
+            selfMeshRenderer.sortingLayerID = SortingLayer.layers[layerValue].id;
 
-            ShowSortingLayer();
+            // 显示 Order in Layer
+            selfMeshRenderer.sortingOrder = EditorGUILayout.IntField("Order in Layer", selfMeshRenderer.sortingOrder);
+        }
+
+        [MenuItem("CONTEXT/MeshFilter/Save Mesh...")]
+        public static void SaveMeshInPlace(MenuCommand menuCommand)
+        {
+            var meshFilter = menuCommand.context as MeshFilter;
+            if (meshFilter!=null)
+            {
+                var mesh = meshFilter.sharedMesh;
+                SaveMesh(mesh, mesh.name, false, true);
+            }
+        }
+
+        [MenuItem("CONTEXT/MeshFilter/Save Mesh As New Instance...")]
+        public static void SaveMeshNewInstanceItem(MenuCommand menuCommand)
+        {
+            var meshFilter = menuCommand.context as MeshFilter;
+            if (meshFilter!=null)
+            {
+                var mesh = meshFilter.sharedMesh;
+                SaveMesh(mesh, mesh.name, true, true);
+            }
         }
         
-        /// <summary>
-        /// 另存为 Mesh
-        /// </summary>
         private static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
         {
             var path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
@@ -247,7 +236,7 @@ namespace Kuroha.Tool.InspectorExtender.Editor
             path = FileUtil.GetProjectRelativePath(path);
 
             var meshToSave = makeNewInstance ? Instantiate(mesh) : mesh;
-            
+
             if (optimizeMesh)
             {
                 MeshUtility.Optimize(meshToSave);
@@ -255,33 +244,6 @@ namespace Kuroha.Tool.InspectorExtender.Editor
 
             AssetDatabase.CreateAsset(meshToSave, path);
             AssetDatabase.SaveAssets();
-        }
-
-        /// <summary>
-        /// 显示 Sorting Layer 信息
-        /// </summary>
-        private void ShowSortingLayer()
-        {
-            if (showLayerValue)
-            {
-                if (selfMeshRenderer != null)
-                {
-                    if (layerNames.Count <= 0 || layerNames.Count != SortingLayer.layers.Length)
-                    {
-                        layerNames.Clear();
-                        layerNames.AddRange(SortingLayer.layers.Select(layer => layer.name));
-                    }
-                    
-                    // 显示 Sorting Layer
-                    var layerValue = SortingLayer.GetLayerValueFromID(selfMeshRenderer.sortingLayerID);
-                    layerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, layerNames.ToArray());
-                    selfMeshRenderer.sortingLayerName = SortingLayer.layers[layerValue].name;
-                    selfMeshRenderer.sortingLayerID = SortingLayer.layers[layerValue].id;
-                    
-                    // 显示 Order in Layer
-                    selfMeshRenderer.sortingOrder = EditorGUILayout.IntField("Order in Layer", selfMeshRenderer.sortingOrder);
-                }
-            }
         }
     }
 }
