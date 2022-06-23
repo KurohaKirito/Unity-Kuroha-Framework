579cbb96224ec161601a3e5dfd4c577eb05f550c
KurohaKirito <KurohaKirito@gmail.com>
2022-06-23 11:14:18 +0800

    批量设置网格

--- Tool/Editor/AssetBatchTool/SetMeshReadWrite.cs
+++ Tool/Editor/AssetBatchTool/SetMeshReadWrite.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Script.Effect.Editor.AssetTool.GUI.Editor;
 using Script.Effect.Editor.AssetTool.Util.Unity;
@@ -9,6 +12,16 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
 {
     public class SetMeshReadWrite
     {
+        private enum FixType
+        {
+            Mesh, Model
+        }
+        
+        private enum RWStatus
+        {
+            开启读写, 关闭读写
+        }
+    
         /// <summary>
         /// 折叠框
         /// </summary>
@@ -39,6 +52,11 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
         /// </summary>
         private const float UI_INPUT_AREA_WIDTH = 400;
 
+        private static FixType fixType;
+        private static string filter = "Collider";
+        private static Regex regex;
+        private static RWStatus flagRW;
+
         /// <summary>
         /// 绘制界面
         /// </summary>
@@ -53,16 +71,32 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
                 GUILayout.Space(UI_DEFAULT_MARGIN);
                 GUILayout.BeginVertical("Box");
                 {
-                    EditorGUILayout.LabelField("1. 输入枪械预制体所在路径.");
+                    EditorGUILayout.LabelField("1. 选择修复类型.");
+                    GUILayout.BeginVertical("Box");
+                    fixType = (FixType) EditorGUILayout.EnumPopup("Input Fix Type", fixType, GUILayout.Width(260));
+                    GUILayout.EndVertical();
+                    
+                    EditorGUILayout.LabelField("2. 输入 Mesh 或者 Model 所在路径.");
                     GUILayout.BeginVertical("Box");
                     checkPath = EditorGUILayout.TextField("Input Path To Detect", checkPath, GUILayout.Width(UI_INPUT_AREA_WIDTH));
                     GUILayout.EndVertical();
+                    
+                    EditorGUILayout.LabelField("3. 选择正确的读写状态.");
+                    GUILayout.BeginVertical("Box");
+                    flagRW = (RWStatus) EditorGUILayout.EnumPopup("Select Target RW", flagRW, GUILayout.Width(260));
+                    GUILayout.EndVertical();
+                    
+                    EditorGUILayout.LabelField("4. 输入文件 (包含路径) 的正则剔除规则");
+                    GUILayout.BeginVertical("Box");
+                    filter = EditorGUILayout.TextField("Input Filter Rule", filter, GUILayout.Width(UI_INPUT_AREA_WIDTH));
+                    GUILayout.EndVertical();
 
-                    EditorGUILayout.LabelField("2. 点击按钮, 开始修复.");
+                    EditorGUILayout.LabelField("5. 点击按钮, 开始修复.");
                     GUILayout.BeginVertical("Box");
                     UnityEngine.GUI.enabled = string.IsNullOrEmpty(checkPath) == false;
                     if (GUILayout.Button("Fix", GUILayout.Height(UI_BUTTON_HEIGHT), GUILayout.Width(UI_BUTTON_WIDTH)))
                     {
+                        InitRegex();
                         FixModel();
                         await FixMesh();
                     }
@@ -74,22 +108,31 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
             }
         }
 
-        private static void FixModel()
+        private static void InitRegex()
         {
-            var counter = 0;
+            regex = new Regex(filter);
+        }
+
+        private static IEnumerable<ModelImporter> GetModelList()
+        {
+            var models = new List<ModelImporter>();
+            var rw = flagRW == RWStatus.开启读写;
             var guids = AssetDatabase.FindAssets("t:Model", new[]
             {
                 checkPath
             });
+            
+            Debug.Log($"一共检测到 {guids.Length} 个 Model");
+            
             for (var index = 0; index < guids.Length; index++)
             {
-                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"Model 检测中: {index + 1}/{guids.Length}", index + 1, guids.Length))
+                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"查找 Model 中: {index + 1}/{guids.Length}", index + 1, guids.Length))
                 {
-                    return;
+                    return models;
                 }
 
                 var path = AssetDatabase.GUIDToAssetPath(guids[index]);
-                if (path.IndexOf("Collider", StringComparison.OrdinalIgnoreCase) > 0)
+                if (regex.Match(path).Success)
                 {
                     continue;
                 }
@@ -98,36 +141,37 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
                 {
                     if (AssetImporter.GetAtPath(path) is ModelImporter modelImporter)
                     {
-                        if (modelImporter.isReadable)
+                        if (modelImporter.isReadable != rw)
                         {
-                            modelImporter.isReadable = false;
-                            modelImporter.SaveAndReimport();
-                            counter++;
+                            models.Add(modelImporter);
                         }
                     }
                 }
             }
 
-            AssetDatabase.Refresh();
-            Debug.Log($"一共检测到 {guids.Length} 个, 共修复了 {counter} 个!");
+            return models;
         }
-
-        private static async Task FixMesh()
+        
+        private static IEnumerable<Mesh> GetMeshList()
         {
-            var counter = 0;
+            var meshes = new List<Mesh>();
+            var rw = flagRW == RWStatus.开启读写;
             var guids = AssetDatabase.FindAssets("t:Mesh", new[]
             {
                 checkPath
             });
+            
+            Debug.Log($"一共检测到 {guids.Length} 个 Mesh");
+
             for (var index = 0; index < guids.Length; index++)
             {
-                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"Mesh 检测中: {index + 1}/{guids.Length}", index + 1, guids.Length))
+                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"查找 Mesh 中: {index + 1}/{guids.Length}", index + 1, guids.Length))
                 {
-                    return;
+                    return meshes;
                 }
 
                 var path = AssetDatabase.GUIDToAssetPath(guids[index]);
-                if (path.IndexOf("Collider", StringComparison.OrdinalIgnoreCase) > 0)
+                if (regex.Match(path).Success)
                 {
                     continue;
                 }
@@ -138,21 +182,63 @@ namespace Script.Effect.Editor.AssetTool.Tool.Editor.AssetBatchTool
                     try
                     {
                         var mesh = AssetDatabase.LoadAssetAtPath<Mesh>(path);
-                        if (mesh.isReadable)
+                        if (mesh.isReadable != rw)
                         {
-                            await mesh.SetReadable(false);
-                            AssetDatabase.Refresh();
-                            counter++;
+                            meshes.Add(mesh);
                         }
                     }
                     catch
                     {
-                        Debug.Log($"无法读取 Mesh, 请检查 Mesh 是否存在问题! {path}");
+                        Debug.Log($"无法将资源读取为 Mesh, 请检查 Mesh 是否存在问题! {path}");
                     }
                 }
             }
 
-            Debug.Log($"一共检测到 {guids.Length} 个, 共修复了 {counter} 个!");
+            return meshes;
+        }
+
+        private static void FixModel()
+        {
+            var counter = 0;
+            var rw = flagRW == RWStatus.开启读写;
+            var models = GetModelList().ToList();
+            
+            for (var index = 0; index < models.Count; index++)
+            {
+                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"Model 修复中: {index + 1}/{models.Count}", index + 1, models.Count))
+                {
+                    return;
+                }
+                
+                models[index].isReadable = rw;
+                models[index].SaveAndReimport();
+                counter++;
+            }
+
+            AssetDatabase.Refresh();
+            Debug.Log($"一共修复了 {counter} 个 Model");
+        }
+
+        private static async Task FixMesh()
+        {
+            var counter = 0;
+            var rw = flagRW == RWStatus.开启读写;
+            var meshes = GetMeshList().ToList();
+            
+            for (var index = 0; index < meshes.Count; index++)
+            {
+                if (ProgressBar.DisplayProgressBarCancel("批处理工具", $"Mesh 修复中: {index + 1}/{meshes.Count}", index + 1, meshes.Count))
+                {
+                    return;
+                }
+                
+                await meshes[index].SetReadable(rw);
+                AssetDatabase.Refresh();
+                counter++;
+            }
+
+            AssetDatabase.Refresh();
+            Debug.Log($"一共修复了 {counter} 个 Mesh");
         }
     }
 }
